175cb704d72a0d843686507f3a8f21f21809b2f1
Akshdeep Sandhu <ashsandhu@alumni.ubc.ca>
2024-07-02 12:26:54 -0700

    updated

--- src/pipeline/FileManager.py
+++ src/pipeline/FileManager.py
@@ -9,13 +9,13 @@ class FileManager:
         self.scratch_space = scratch_space
         self.patient_id = network_drive.split('/')[-3]
 
-    def copyFiles(self, patient_id):
-        print(f"Copying files for patient {patient_id} from {self.network_drive} to {self.scratch_space}.")
+    def copyFiles(self):
+        print(f"Copying files for patient {self.patient_id} from {self.network_drive} to {self.scratch_space}.")
 
-    def copyDICOM(self, patient_id):
+    def copyDICOM(self):
         print(f"Copying DICOM for patient {patient_id} from {source_path} to {dest_path}.")
 
-    def delFiles(self, patient_id):
+    def delFiles(self):
         print(f"Deleting files for patient {patient_id} in {data_path}.")
 
 if __name__ == "__main__":

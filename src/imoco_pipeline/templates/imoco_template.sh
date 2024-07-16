f204ebef4001d616f436bbceb87b36b69c3a10ca
Akshdeep Sandhu <ash.sandhu@bcchr.ca>
2024-07-16 09:12:49 -0700

    fixed issue with imoco tempalte

--- src/imoco_pipeline/templates/imoco_template.sh
+++ src/imoco_pipeline/templates/imoco_template.sh
@@ -11,10 +11,10 @@ module load singularity cuda11.4/toolkit/11.4.2
 # execute imoco pipeline
 singularity exec --nv --bind {SCRATCH_PATH}:/container_data /mnt/scratch/Precision/BioStats/ASandhu/images/imoco_gpu_latest.sif bash -c "
 source /usr/local/.gpu_venv/bin/activate
-file_dir=/container_data/{SCAN_ID}/processed_data
-imoco_dir=/usr/src/imoco_recon/imoco_py
-python3 \$/convert_uwute.py \${file_dir}/MRI_Raw
-python3 \$/recon_xdgrasp.py \${file_dir}/MRI_Raw
-python3 \$/recon_imoco.py \${file_dir}/MRI_Raw --reg_flag 1 --device 0
-python3 \$/dicom_creation.py \${file_dir}
+file_dir=/container_data/processed_data
+cd /usr/src/imoco_recon/imoco_py
+python3 convert_uwute.py \${file_dir}/MRI_Raw
+python3 recon_xdgrasp.py \${file_dir}/MRI_Raw
+python3 recon_imoco.py \${file_dir}/MRI_Raw --reg_flag 1 --device 0
+python3 dicom_creation.py \${file_dir}
 "

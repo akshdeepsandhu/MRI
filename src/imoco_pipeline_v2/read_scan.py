07bcf39c9b96b455e52089ab18f2c53cd21c8991
Akshdeep Sandhu <ash.sandhu@bcchr.ca>
2024-11-19 15:44:22 -0800

    new pipeline dev

--- src/imoco_pipeline_v2/read_scan.py
+++
@@ -1,4 +0,0 @@
-from minio import Minio
-client = Minio("192.168.21.61:9000" , access_key="minioadmin", secret_key="minioadmin", secure=False)
-
-bucket_name="imrh-data"
---
+++ src/imoco_pipeline_v2/scan.py
@@ -0,0 +1,60 @@
+import os 
+import logging 
+import shutil 
+import subprocess
+import io
+
+class Scan: 
+    def __init__(self, scan_id, data_path):
+        self.scan_id = scan_id
+        self.data_path = data_path
+        self.h5_file_path = self._set_h5_file_path()
+
+
+    def _set_h5_file_path(self) -> str: 
+        for file in os.listdir(self.data_path):
+            if file.endswith('.h5') and not file.endswith('Raw.h5'):
+                logging.info(f"Found .h5 file: {file}")
+                return f'{self.data_path}/{file}'
+            else: 
+                logging.warning("No .h5 file found in the copied folder.")
+                return None
+            
+
+    def make_script_content(self, template_path: str, replacements: dict) -> str:
+
+        with open(template_path, 'r') as template_file:
+            template_content = template_file.read()
+
+        for placeholder, value in replacements.items():
+            template_content = template_content.replace(placeholder, value)
+
+        return template_content
+
+    def write_pcvipr_script(self, write_to_file=True) -> None:
+
+        self.pcvipr_script_name = f"{self.scan_id}_pcvipr.sh"
+
+        script_content = self.make_script_content(
+            template_path="templates/pcvipr_template.sh",
+            replacements={
+                '{SCAN_DATA_PATH}': self.data_path,
+                '{H5_FILE_NAME}': self.h5_file_path
+            }
+        )
+
+        if write_to_file:
+            script_path = os.path.join(self.data_path, self.pcvipr_script_name)
+            with open(script_path, 'w') as script_file:
+                script_file.write(script_content)
+            os.chmod(script_path, 0o755)
+            logging.info(f"Script written to file: {script_path}")
+
+
+
+if __name__ == '__main__':
+    data_path = '/mnt/scratch/Precision/BioStats/ASandhu/imrh_warehouse/db/imrh-data/iMRH0128B'
+    test_scan = Scan('iMRH0128B', data_path)
+    print(test_scan.h5_file_path)
+    # add to bucket
+    test_scan.write_pcvipr_script(write_to_file=True)
